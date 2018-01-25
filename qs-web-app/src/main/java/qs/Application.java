fb95e8954215598725f5da8080b40f2e5ed1de4e
yinqingzhun <yinqingzhun@autohome.com.cn>
2018-01-25 10:15:49 +0800

    load time weaving with multiple modules

---
+++ qs-web-app/src/main/java/qs/Application.java
@@ -0,0 +1,38 @@
+package qs;
+
+import de.invesdwin.instrument.DynamicInstrumentationLoader;
+import org.aspectj.lang.Aspects;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.ErrorMvcAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.EnableLoadTimeWeaving;
+import org.springframework.context.annotation.ImportResource;
+import qs.config.db.DataSourceAspect;
+
+
+/**
+ * Created by yinqingzhun on 2017/08/29.
+ */
+@SpringBootApplication(exclude = {ErrorMvcAutoConfiguration.class})
+@EnableLoadTimeWeaving(aspectjWeaving = EnableLoadTimeWeaving.AspectJWeaving.ENABLED)
+public class Application extends SpringBootServletInitializer {
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        DynamicInstrumentationLoader.waitForInitialized(); //dynamically attach java agent to jvm if not already present
+        DynamicInstrumentationLoader.initLoadTimeWeavingContext(); //weave all classes before they are loaded as beans
+        return builder.sources(Application.class);
+    }
+
+    public static void main(String[] args) {
+        DynamicInstrumentationLoader.waitForInitialized(); //dynamically attach java agent to jvm if not already present
+        DynamicInstrumentationLoader.initLoadTimeWeavingContext(); //weave all classes before they are loaded as beans
+        SpringApplicationBuilder builder = new SpringApplicationBuilder();
+        builder.sources(Application.class).run();
+    }
+
+
+}
---
+++ qs-web-app/src/main/java/qs/web/WebControllerAdvice.java
@@ -0,0 +1,106 @@
+package qs.web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.annotation.AnnotationUtils;
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
+import qs.exception.SupportInfoException;
+
+import javax.servlet.http.HttpServletRequest;
+import java.sql.SQLException;
+import java.util.Date;
+
+/**
+ * Created by yinqingzhun on 2017/08/29.
+ */
+@ControllerAdvice
+public class WebControllerAdvice {
+    Logger logger= LoggerFactory.getLogger(WebControllerAdvice.class);
+
+   /* @ExceptionHandler(exception.class)
+    @ResponseBody
+    ResponseEntity<?> handleControllerException(HttpServletRequest request, Throwable ex) {
+        HttpStatus status = getStatus(request);
+        return new ResponseEntity<>(ReturnValue.buildErrorResult(status.value(), ex.getMessage()), status);
+    }*/
+
+
+
+    private HttpStatus getStatus(HttpServletRequest request) {
+        Integer statusCode = (Integer) request.getAttribute("javax.servlet.error.status_code");
+        if (statusCode == null) {
+            return HttpStatus.INTERNAL_SERVER_ERROR;
+        }
+        return HttpStatus.valueOf(statusCode);
+    }
+
+    /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+	/* . . . . . . . . . . . . . EXCEPTION HANDLERS . . . . . . . . . . . . .. */
+	/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+    /**
+     * Convert a predefined exception to an HTTP Status code
+     */
+    @ResponseStatus(value = HttpStatus.CONFLICT, reason = "Data integrity violation")
+    // 409
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public void conflict() {
+        logger.error("Request raised a DataIntegrityViolationException");
+        // Nothing to do
+    }
+
+    /**
+     * Convert a predefined exception to an HTTP Status code and specify the
+     * name of a specific view that will be used to display the error.
+     *
+     * @return exception view.
+     */
+    @ExceptionHandler({ SQLException.class, DataAccessException.class })
+    public String databaseError(Exception exception) {
+        // Nothing to do. Return value 'databaseError' used as logical view name
+        // of an error page, passed to view-resolver(s) in usual way.
+        logger.error("Request raised " + exception.getClass().getSimpleName());
+        return "error";
+    }
+
+    /**
+     * Demonstrates how to take total control - setup a model, add useful
+     * information and return the "support" view name. This method explicitly
+     * creates and returns
+     *
+     * @param req
+     *            Current HTTP request.
+     * @param exception
+     *            The exception thrown - always {@link SupportInfoException}.
+     * @return The model and view used by the DispatcherServlet to generate
+     *         output.
+     * @throws Exception
+     */
+    @ExceptionHandler(SupportInfoException.class)
+    public ModelAndView handleError(HttpServletRequest req, Exception exception)
+            throws Exception {
+
+        // Rethrow annotated exceptions or they will be processed here instead.
+        if (AnnotationUtils.findAnnotation(exception.getClass(),
+                ResponseStatus.class) != null)
+            throw exception;
+
+        logger.error("Request: " + req.getRequestURI() + " raised " + exception);
+
+        ModelAndView mav = new ModelAndView();
+        mav.addObject("exception", exception);
+        mav.addObject("url", req.getRequestURL());
+        mav.addObject("timestamp", new Date().toString());
+        mav.addObject("status", 500);
+
+        mav.setViewName("support");
+        return mav;
+    }
+
+}
